ac76d9ca91cc9bfded82620fc376e1a073c2aeee
monteluka <dmonterrosa@pm.me>
2024-09-14 19:46:43 -0700

    Get process id for pvz

--- PVZExternalMultiHack/PVZExternalMultiHack.cpp
+++ PVZExternalMultiHack/PVZExternalMultiHack.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
 
-int main(int argc, char* argv[])
+int main()
 {
-    std::cout << "Hello World!" << std::endl; 
+    // get process id
+    HANDLE hSnapshot{CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)};
+
+    // need to get the size according to MSDN
+    PROCESSENTRY32 pe32{};
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    // loop through all the processes in hSnapshot
+    do
+    {
+        if (!_wcsicmp(pe32.szExeFile, L"popcapgame1.exe")) break;
+    }
+    while (Process32Next(hSnapshot, &pe32));
+    CloseHandle(hSnapshot);
+
+    // display information on the process
+    std::wcout << "PVZ Process ID: " << pe32.th32ProcessID << '\n' << "Process name: " << pe32.szExeFile;
+
     return 0;
 }
