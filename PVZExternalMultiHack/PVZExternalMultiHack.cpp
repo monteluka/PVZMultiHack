e92c85b0f0d0ae6605b5e54e994ff42fdccaa952
monteluka <dmonterrosa@pm.me>
2024-09-15 10:45:59 -0700

    Set up skeleton for hack options

--- PVZExternalMultiHack/PVZExternalMultiHack.cpp
+++ PVZExternalMultiHack/PVZExternalMultiHack.cpp
@@ -2,6 +2,25 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
+// global options
+struct sHacks
+{
+    bool autoCollectItems{false};
+    bool bypassSunLimit{false};
+    bool fastSunProduction{false};
+    bool hitAnywhere{false};
+    bool infiniteCoins{false};
+    bool infiniteLawnMower{false};
+    bool infiniteSun{false};
+    bool infinitePlantHealth{false};
+    bool instantActivatePotatoMine{false};
+    bool instantPlantRecharge{false};
+    bool noChomperCooldown{false};
+    bool noPlantRestriction{false};
+    bool noZombies{false};
+    bool oneHitKills{false};
+} hacks;
+
 int main()
 {
     // get process id
@@ -23,14 +42,98 @@ int main()
     std::wcout << "PVZ Process ID: " << pe32.th32ProcessID << '\n' << "Process name: " << pe32.szExeFile << '\n';
 
     // get handle to pvz
-    HANDLE hPVZ {OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID)};
+    HANDLE hPVZ{OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID)};
 
-    // check if handle is valid and we can read sun amount
-    DWORD sun {};
-    std::uintptr_t sunAddress {0x21C851A8};
-    ReadProcessMemory(hPVZ, reinterpret_cast<LPVOID>(sunAddress), &sun, sizeof(sun), nullptr);
-    std::cout << "Current amount of sun: " << sun;
-    
+    // start hack loop
+    for (;;)
+    {
+        // skeleton for hacks
+        if (hacks.autoCollectItems)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.bypassSunLimit)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.fastSunProduction)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.hitAnywhere)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.infiniteCoins)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.infiniteLawnMower)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.infiniteSun)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.instantActivatePotatoMine)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.infinitePlantHealth)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.instantPlantRecharge)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.noChomperCooldown)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.noPlantRestriction)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.noZombies)
+        {
+        }
+        else
+        {
+        }
+        if (hacks.oneHitKills)
+        {
+        }
+        else
+        {
+        }
+    }
+
     CloseHandle(hPVZ);
     return 0;
 }
