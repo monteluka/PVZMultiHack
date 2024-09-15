1f53b749e919733e63b9ae22447e76dbf5efdad4
monteluka <dmonterrosa@pm.me>
2024-09-14 20:03:36 -0700

    Create a handle for pvz
    also checked to see if handle is valid

--- PVZExternalMultiHack/PVZExternalMultiHack.cpp
+++ PVZExternalMultiHack/PVZExternalMultiHack.cpp
@@ -20,7 +20,17 @@ int main()
     CloseHandle(hSnapshot);
 
     // display information on the process
-    std::wcout << "PVZ Process ID: " << pe32.th32ProcessID << '\n' << "Process name: " << pe32.szExeFile;
+    std::wcout << "PVZ Process ID: " << pe32.th32ProcessID << '\n' << "Process name: " << pe32.szExeFile << '\n';
 
+    // get handle to pvz
+    HANDLE hPVZ {OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID)};
+
+    // check if handle is valid and we can read sun amount
+    DWORD sun {};
+    std::uintptr_t sunAddress {0x21C851A8};
+    ReadProcessMemory(hPVZ, reinterpret_cast<LPVOID>(sunAddress), &sun, sizeof(sun), nullptr);
+    std::cout << "Current amount of sun: " << sun;
+    
+    CloseHandle(hPVZ);
     return 0;
 }
