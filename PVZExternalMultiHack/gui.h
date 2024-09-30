96a1b233344ef566377812f3712b60e3ac5c1732
monteluka <dmonterrosa@pm.me>
2024-09-29 23:38:28 -0700

    Created gui namespace
    Namespace holds the functions and variables needed to create, render,
    and destroy window for hack
    Functions and variables were pulled from ImGui example

---
+++ PVZExternalMultiHack/gui.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_win32.h"
+#include "imgui/imgui_impl_dx11.h"
+#include <d3d11.h>
+
+namespace gui
+{
+    // Data
+    inline ID3D11Device* g_pd3dDevice {nullptr};
+    inline ID3D11DeviceContext* g_pd3dDeviceContext {nullptr};
+    inline IDXGISwapChain* g_pSwapChain {nullptr};
+    inline bool g_SwapChainOccluded {false};
+    inline UINT g_ResizeWidth {0};
+    inline UINT g_ResizeHeight {0};
+    inline ID3D11RenderTargetView* g_mainRenderTargetView {nullptr};
+
+    // globals for window
+    inline WNDCLASSEXW wc;
+    inline HWND hwnd;
+    inline constexpr int WINDOW_WIDTH = 240; 
+    inline constexpr int WINDOW_HEIGHT = 160; 
+    
+
+    bool CreateDeviceD3D();
+    void CleanupDeviceD3D();
+    void CreateRenderTarget();
+    void CleanupRenderTarget();
+    LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+    void CreateAppWindow(const wchar_t* windowName, const wchar_t* className);
+    void DestroyAppWindow();
+
+    void CreateImGui();
+    void DestroyImGui();
+
+    void BeginRender();
+    void EndRender();
+
+}
